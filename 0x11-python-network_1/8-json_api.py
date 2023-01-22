2d2bb3eee77809db5c76b169d6d514db63772c71
Francis1104OL <taiwofrancis001@gmail.com>
2023-01-22 03:41:03 -0800

    all

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
+import requests
 
 
-def searchapi():
-    """status"""
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        data = result.json()
-        if data:
-            print("[{}] {}".format(data["id"], data["name"]))
-        else:
+        response = r.json()
+        if response == {}:
             print("No result")
-    except:
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
         print("Not a valid JSON")
-
-        if __name__ == "__main__":
-            searchapi()
